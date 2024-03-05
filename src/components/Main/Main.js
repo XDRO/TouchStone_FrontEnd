036243e9e83e99552f4eedfbee7f54c4db1c0b0f
XDRO <joshuaarcher76@gmail.com>
2024-03-05 17:46:14 -0600

    add width properties to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,8 +4,12 @@ import TSBackGround from "../../images/TouchStone-LandingPage-Background.svg";
 export const Main = () => {
   return (
     <main className="main">
-      <div className="main__background">
-        <img src={TSBackGround} alt="background"></img>
+      <div className="main__content">
+        <img
+          className="main__content main__content-background"
+          src={TSBackGround}
+          alt="background"
+        ></img>
       </div>
     </main>
   );
