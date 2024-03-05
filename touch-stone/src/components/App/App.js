2fcf873dad04e45f3507a28afd0b12f15d64afeb
XDRO <joshuaarcher76@gmail.com>
2024-03-05 11:43:05 -0600

    Add comments of some next steps to take

--- touch-stone/src/components/App/App.js
+++ touch-stone/src/components/App/App.js
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // put all consts, functions, and useEffects here
+  // return
+  // in return implement at least two routes
+  // here is what i believe i will need for routes
+  // Route path = "/" for Main
+  // Route path = "/register"
+  // Route path = "/login"
+  // Protected Route path = "/profile" w Profile inside
+
   return (
     <div className="App">
       <header className="App-header">
