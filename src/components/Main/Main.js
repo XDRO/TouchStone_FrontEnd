53e6877f602202011517a5eeaaf597645a0f5a23
XDRO <joshuaarcher76@gmail.com>
2024-03-05 17:23:37 -0600

    add return statement to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import "./Main.css";
 import TSBackGround from "../../images/TouchStone-LandingPage-Background.svg";
 
 export const Main = () => {
-  <main className="main">
-    <div className="main__background">
-      <img src={TSBackGround} alt="background"></img>
-    </div>
-    <div>some text</div>
-  </main>;
+  return (
+    <main className="main">
+      <div className="main__background">
+        <img src={TSBackGround} alt="background"></img>
+      </div>
+    </main>
+  );
 };
