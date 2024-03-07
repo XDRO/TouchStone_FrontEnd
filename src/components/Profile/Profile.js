6289000a0faa49e73e01912eca56d12a2599a073
XDRO <joshuaarcher76@gmail.com>
2024-03-07 13:35:32 -0600

    continue style and set up for searchbar in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,13 @@
 import "./Profile.css";
 import { ProfileSideBar } from "./ProfileSideBar";
+import { ProfileSearchBar } from "./ProfileSearchBar";
 export const Profile = () => {
   return (
     <div className="profile">
       {/* create sidebar.js for profile questions */}
       <div className="profile__content">
         <ProfileSideBar></ProfileSideBar>
+        <ProfileSearchBar></ProfileSearchBar>
       </div>
     </div>
   );
--- src/components/Profile/ProfileSearchBar.js
+++ src/components/Profile/ProfileSearchBar.js
@@ -1,1 +1,24 @@
-export const ProfileSearchBar = () => {};
+import SoundWaves from "../../images/TouchStone-Sound-Waves.svg";
+export const ProfileSearchBar = () => {
+  return (
+    <div className="profile__searchbar">
+      <div className="profile__searchbar-content">
+        <label
+          className="profile__searchbar-label"
+          htmlFor="profile__searchbar-text"
+        >
+          Ask TouchStone
+        </label>
+        <textarea
+          //   placeholder="Ask TouchStone"
+          className="profile__searchbar-text"
+          rows={1}
+          cols={1}
+        ></textarea>
+        <button className="profile__searchbar-speach">
+          <img src={SoundWaves} alt="Voice Commands"></img>
+        </button>
+      </div>
+    </div>
+  );
+};
