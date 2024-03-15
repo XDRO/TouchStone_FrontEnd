ebe706ae98ba41bf32cc71baf949631528a91f51
XDRO <joshuaarcher76@gmail.com>
2024-03-15 10:50:21 -0500

    connect font-family to css files, and import normalize.css as well as fonts.css into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "../src/vendor/normalize.css";
+import "../src/vendor/fonts.css";
 import App from "../src/components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
