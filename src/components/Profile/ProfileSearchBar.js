d3ddec277f69e314d2ccd0fe331f2c30e6896375
jmjcoding <jmjcoding@icloud.com>
2024-07-05 17:03:37 -0500

    add pulse animation to submit button

--- src/components/Profile/ProfileSearchBar.js
+++ src/components/Profile/ProfileSearchBar.js
@@ -1,4 +1,4 @@
-import SoundWaves from "../../images/TouchStone-Sound-Waves.svg";
+import SubmitButton from "../../images/Submit-Button.svg";
 import React, { useState } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 // add code blocks for users inquireing about codiing
@@ -12,7 +12,11 @@ export const ProfileSearchBar = ({ onAddUserMessage, activeChat }) => {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      await onAddUserMessage({ chatId: activeChat?._id, text: message });
+      if (message === "") {
+        console.log("Please enter a message to send");
+      } else {
+        await onAddUserMessage({ chatId: activeChat?._id, text: message });
+      }
       setMessage("");
     } catch (error) {
       console.error("Error from handleSubmit in profileSearchBar :", error);
@@ -52,18 +56,23 @@ export const ProfileSearchBar = ({ onAddUserMessage, activeChat }) => {
           <input
             placeholder="Ask TouchStone:"
             className="profile__searchbar-text"
-            onSubmit={handleSubmit}
             onChange={handleMessageChange}
             onKeyDown={onKeyDown}
             value={message}
           ></input>
         </form>
 
-        <button className="profile__searchbar-speach" type="submit">
+        <button className="profile__searchbar-submit" type="submit">
           {/* conditionally render sound waves or 
           submit button based on if text is in textarea
           if message does not equal '' then send button else soundwaves */}
-          <img src={SoundWaves} alt="Voice Commands"></img>
+          <img
+            className="profile__searchbar-submit-button"
+            src={SubmitButton}
+            alt="Voice Commands"
+            type="submit"
+            onClick={handleSubmit}
+          ></img>
         </button>
       </div>
     </div>
