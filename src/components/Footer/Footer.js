ed312940fad386d467d21e620c710130b5298fd4
jmjcoding <jmjcoding@icloud.com>
2024-07-06 10:49:49 -0500

    get profile adjusted for 844px scren width

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer = ({ onClick }) => {
     <footer className="footer">
       <div className="footer__options">
         <div onClick={() => onClick("about")} className="footer_about">
-          About Us
+          About
         </div>
         <div onClick={() => onClick("contact")} className="footer_contact">
           Contact
