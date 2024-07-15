7b65345e2222e53c54e59271d1a9b8d67d61c611
jmjcoding <jmjcoding@icloud.com>
2024-07-15 11:11:51 -0500

    get rid of empty div tags except for in modalEditProfile in case I come back to it, and add it to the application

--- src/components/ModalAbout/ModalAbout.js
+++ src/components/ModalAbout/ModalAbout.js
@@ -4,63 +4,60 @@ import "./ModalAbout.css";
 export const ModalAbout = ({ handleCloseModal }) => {
   return (
     <Modal onClose={handleCloseModal} name={`about`}>
-      <div>
-        <h3 className={`modal__title modal__title_${"about"}`}>About</h3>
-        <p className={`modal__text modal__text_${"about"}`}>
-          Welcome to my project showcase! This endeavor represents the
-          culmination of my journey at TripleTen and is my final project before
-          graduation. If you're curious about TripleTen, passionate about
-          learning to code, or eager to enter the tech industry, feel free to
-          explore more at:
-          <a
-            className={`modal__link_${"about"}`}
-            href="https://tripleten.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://tripleten.com/
-          </a>
-        </p>
-        <p className={`modal__text modal__text_${"about"}`}>
-          Technologies utilized: React, Node.js, Express, MongoDB, and the
-          OpenAI API. If you're interested in harnessing the power of the OpenAI
-          API for your projects, you can find comprehensive documentation here:
-          <a
-            className={`modal__link_${"about"}`}
-            href="https://platform.openai.com/docs/api-reference/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://platform.openai.com/docs/api-reference/introduction
-          </a>
-        </p>
+      <h3 className={`modal__title modal__title_${"about"}`}>About</h3>
+      <p className={`modal__text modal__text_${"about"}`}>
+        Welcome to my project showcase! This endeavor represents the culmination
+        of my journey at TripleTen and is my final project before graduation. If
+        you're curious about TripleTen, passionate about learning to code, or
+        eager to enter the tech industry, feel free to explore more at:
+        <a
+          className={`modal__link_${"about"}`}
+          href="https://tripleten.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://tripleten.com/
+        </a>
+      </p>
+      <p className={`modal__text modal__text_${"about"}`}>
+        Technologies utilized: React, Node.js, Express, MongoDB, and the OpenAI
+        API. If you're interested in harnessing the power of the OpenAI API for
+        your projects, you can find comprehensive documentation here:
+        <a
+          className={`modal__link_${"about"}`}
+          href="https://platform.openai.com/docs/api-reference/introduction"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://platform.openai.com/docs/api-reference/introduction
+        </a>
+      </p>
 
-        <p className={`modal__text modal__text_${"about"}`}>
-          To dive deeper into this project, please visit my GitHub repositories:
-          <br></br>
-        </p>
+      <p className={`modal__text modal__text_${"about"}`}>
+        To dive deeper into this project, please visit my GitHub repositories:
+        <br></br>
+      </p>
 
-        <p className={`modal__text modal__text_${"about"}`}>
-          <a
-            className={`modal__link_${"about"}`}
-            href="https://github.com/XDRO/TouchStone_FrontEnd"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://github.com/XDRO/TouchStone_FrontEnd
-          </a>
-        </p>
-        <p className={`modal__text modal__text_${"about"}`}>
-          <a
-            className={`modal__link_${"about"}`}
-            href="https://github.com/XDRO/TouchStone_BackEnd"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://github.com/XDRO/TouchStone_BackEnd
-          </a>
-        </p>
-      </div>
+      <p className={`modal__text modal__text_${"about"}`}>
+        <a
+          className={`modal__link_${"about"}`}
+          href="https://github.com/XDRO/TouchStone_FrontEnd"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/XDRO/TouchStone_FrontEnd
+        </a>
+      </p>
+      <p className={`modal__text modal__text_${"about"}`}>
+        <a
+          className={`modal__link_${"about"}`}
+          href="https://github.com/XDRO/TouchStone_BackEnd"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/XDRO/TouchStone_BackEnd
+        </a>
+      </p>
     </Modal>
   );
 };
